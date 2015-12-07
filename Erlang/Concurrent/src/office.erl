92d945c3eacc4119673dbf2bb9adeda04582a25a
sellnat77 <sell_nat@yahoo.com>
2015-12-06 16:12:24 -0800

    Working on student side

--- Erlang/Concurrent/src/office.erl
+++ Erlang/Concurrent/src/office.erl
@@ -1,7 +1,7 @@
 -module(office).
--export([room/3, student/2, officedemo/0, busyOffice/0, checkFront/2, putTail/2,indexOf/2]).
+-export([room/4, student/2, officedemo/0, busyOffice/0, checkFront/2, putTail/2,indexOf/2]).
 
-room(Students, Capacity, Queue) ->
+room(Students, Capacity, Queue,Helping) ->
   receive
     %check if student is at front of queue
     %If the queue is empty, student can be admitted automatically
@@ -14,12 +14,12 @@ room(Students, Capacity, Queue) ->
         %student is at front of queue or there is no queue
         true ->
           From ! {self(), ok},
-          room([Name|Students], Capacity - 1,lists:delete(Name, Queue));
+          room([Name|Students], Capacity - 1,lists:delete(Name, Queue),Helping);
         %student is not at front of queue
         false->
-          QueueWait = 1000 * office:indexOf(Name,Queue),
+          QueueWait = 1000 * office:indexOf(Name,[Queue]),
           From ! {self(), room_full, QueueWait},
-          room([Students], Capacity, Queue)
+          room([Students], Capacity, [Queue],Helping)
         end;
 
 
@@ -30,14 +30,14 @@ room(Students, Capacity, Queue) ->
         true ->
           QueueWait = 1000 * office:indexOf(Name,Queue),
           From ! {self(), ok, QueueWait},
-          room([Students], Capacity, Queue);
+          room([Students], Capacity, [Queue],Helping);
           %student is not in the queue
         false->
           %need to construct the queue appropriately with reverse
           NewQueue = office:putTail(Name,Queue),
           QueueWait = 1000 * office:indexOf(Name,NewQueue),
           From ! {self(), room_full, QueueWait},
-          room([Students], Capacity, NewQueue)
+          room([Students], Capacity, [NewQueue],Helping)
         end;
       %If the student is in the queue reply with message giving the sleep time for this student
 
@@ -45,43 +45,73 @@ room(Students, Capacity, Queue) ->
       %must use reverse to append at end of list
       %modify wait time for each student based on their postion in the queue
 
+
+    {From, help_me, Name} ->
+      case Helping of
+        true->
+          From ! {self(), busy},
+          room([Students],Capacity,[Queue],Helping);
+        false->
+          From ! {self(), ok},
+          room([Students],Capacity,[Queue],true)
+      end;
+
     % student leaving
     {From, leave, Name} ->
       % make sure they are already in the room
       case lists:member(Name, Students) of
         true ->
           From ! {self(), ok},
-          room(lists:delete(Name, Students), Capacity + 1,Queue);
+          room(lists:delete(Name, Students), Capacity + 1,[Queue],false);
         false ->
           From ! {self(), not_found},
-          room(Students, Capacity,Queue)
-      end
+          room([Students], Capacity,[Queue],false)
+      end;
+
+    % student thanks
+    {From, thanks, Name} ->
+      room([Students],Capacity,Queue,false)
 end.
 
 studentWork(Name) ->
-  SleepTime = rand:uniform(7000) + 3000,
+  SleepTime = rand:uniform(5000) + 5000,
   io:format("~s entered the Office and will work for ~B ms.~n", [Name, SleepTime]),
   timer:sleep(SleepTime).
 
 student(Office, Name) ->
   timer:sleep(rand:uniform(3000)),
-  Office ! {self(), help_me, Name},
   Office ! {self(), enter, Name},
+  Office ! {self(), help_me, Name},
   receive
     % Success; can enter room.
     {_, ok} ->
       studentWork(Name),
       Office ! {self(), leave, Name},
+      Office ! {self(), thanks, Name},
       io:format("~s left the Office.~n", [Name]);
+
+  % Success; can enter room.
+    {_, ok, SleepTime} ->
+      studentWork(Name),
+      Office ! {self(), thanks, Name},
+      io:format("~s left the Office.~n", [Name]);
+
+  % Instructor is busy
+    {_, busy} ->
+      office:busyOffice(),
+      io:format("~s wanted help but the instructor was busy, asking again.~n", [Name]),
+      Office ! {self(), help_me, Name},
+      student(Office, Name);
 
     % Office is full; sleep and try again.
     {_, room_full, SleepTime} ->
-      office:busyOffice(),
+      io:format("~s could not enter and must wait ~B ms.~n", [Name, SleepTime]),
+      timer:sleep(SleepTime),
       student(Office, Name)
   end.
 
 busyOffice() ->
-  io:format("Instructor is busy."),
+  io:format("Instructor is busy.~n"),
   timer:sleep(1000).
 
 checkFront(_,[]) -> true;
@@ -92,10 +122,10 @@ putTail(Element, List) -> lists:reverse([Element |lists:reverse(List)]).
 
 indexOf(_, []) -> notFound;
 indexOf(Val,[Val|_]) -> 1;
-indexOf(Val,[_|T]) -> 1+indexOf(Val,T).
+indexOf(Val,[_|T]) -> 1+indexOf(Val,[T]).
 
 officedemo() ->
-  R = spawn(office, room, [[], 3,[]]), % start the room process with an empty list of students
+  R = spawn(office, room, [[], 3,[],false]), % start the room process with an empty list of students
   spawn(office, student, [R, "Ada"]),
   spawn(office, student, [R, "Barbara"]),
   spawn(office, student, [R, "Charlie"]),
